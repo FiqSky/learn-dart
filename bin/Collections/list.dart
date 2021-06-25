61ca972daaa8a9aacde8655d86967fc8c72c684f
FiqSky <fiqihfm05@gmail.com>
2021-06-25 10:05:50 +0700

    collections clear

---
+++ bin/Collections/list.dart
@@ -0,0 +1,6 @@
+void main(){
+  var favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
+  var others = ['Cake', 'Pie', 'Donut'];
+  var allFavorites = [...favorites, ...others]; //spread operator
+  print(allFavorites);
+}

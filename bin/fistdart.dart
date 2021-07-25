e33a3bfbe42e5cc0bd779f39aeaded0c8f597b8c
FiqSky <fiqihfm05@gmail.com>
2021-07-25 13:35:12 +0700

    finish dart fundamental

--- bin/fistdart.dart
+++ bin/fistdart.dart
@@ -1,9 +1,10 @@
-void main() {
-  try {
-    print('Fetching username...');
-    var username = await fetchUsername();
-    print('You are logged in as $username');
-    print('Welcome!');
-  } catch(e) {
-    print(e);
-  }
+Future<void> main() async {
+  print('Fetching username...');
+  var username = fetchUsername();
+  print('You are logged in as $username');
+  print('Welcome!');
+}
+
+Future<String> fetchUsername() {
+  return Future.delayed(Duration(seconds: 3), () => 'DartUser');
+}
